84f64ea1a24f725d8dc10dcabf5dd6ebc31cf676
Nilton <newtonesoft@gmail.com>
2018-02-25 12:39:16 -0300

    - Adding local server file

---
+++ server_local.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express       = require('express'),
+    app           = express(),
+    mongojs       = require('mongojs'),
+    bodyParser    = require('body-parser'),
+    passport      = require('passport'),
+    cookieParser  = require('cookie-parser'),
+    session       = require('express-session'),
+    flash         = require('connect-flash'),
+    port          = 9000;
+
+var db = mongojs('app'); // Database name
+
+require('./config/passport')(passport, db);
+
+app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs-locals'));
+app.use(flash());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'smellycatsmellycat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./app/dtbase.js')(app, db);
+require('./app/routes.js')(app, passport, db);
+
+app.listen(port, function() {
+    console.log('%s: Servidor iniciado em %s...',
+        Date(Date.now()), porta);
+});
